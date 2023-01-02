d40d54248f3cffcd88f31f3495525191a87820f2
zacard.lin <13816025562@139.com>
2023-01-02 20:28:41 +0800

    feat: add block style

--- src/components/ToolBarTop/ToolBarTop.tsx
+++ src/components/ToolBarTop/ToolBarTop.tsx
@@ -7,10 +7,13 @@ import useRouteParam from '@/hooks/useRouteParam'
 export default defineComponent({
   name: 'ToolBarTop',
   setup() {
-    const { topic } = useRouteParam()
+    const { topic, path } = useRouteParam()
 
     return () => (
-      <div className="toolbar-top">
+      <div
+        className="toolbar-top"
+        style={{ paddingLeft: path.value.includes('block-') ? '12px' : '0' }}
+      >
         <div>{`${topic.value ?? ''}`}</div>
       </div>
     )
